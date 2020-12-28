9cb8c2856713ac80750871b117c8391e3cf72a62
Mihalis Rasoulis <rassmike@gmail.com>
2020-12-28 11:51:08 +0200

    Add docs.

--- shell.nix
+++ shell.nix
@@ -2,9 +2,8 @@ let
   sources = import ./nix/sources.nix {};
   pkgs = import sources.nixpkgs {};
   haskellEnv = pkgs.haskell.packages.ghc865.ghcWithPackages
-    (p: with p; [
-      process
-      random
+    (haskellPkgs: with haskellPkgs; [
+      yesod
       purescript
       spago
     ]);
@@ -13,6 +12,7 @@ pkgs.mkShell {
   buildInputs = [
     haskellEnv
     pkgs.cowsay
+    pkgs.postgresql
   ];
   PORT = 3000;
 }
